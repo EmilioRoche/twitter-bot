4f1f3a1feb1211e8b6dba6aa6d5db6c1dbd18819
EmilioRoche <32501779+EmilioRoche@users.noreply.github.com>
2022-10-19 23:11:27 -0600

    setup new keys

--- src/main.py
+++ src/main.py
@@ -1,23 +1,22 @@
 import os
-from apiCall import get_fruit 
+import tweepy
 
 from dotenv import load_dotenv
 load_dotenv()
 
-API_KEY = os.getenv('API_KEY')
-API_KEY_SECRET = os.getenv('API_KEY_SECRET')
-ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
-ACCESS_TOKEN_SECRET = os.getenv('ACCESS_TOKEN_SECRET')
+API_KEY = os.getenv('TWITTER_API_KEY')
+API_KEY_SECRET = os.getenv('TWITTER_API_KEY_SECRET')
+ACCESS_TOKEN = os.getenv('TWITTER_ACCESS_TOKEN')
+ACCESS_TOKEN_SECRET = os.getenv('TWITTER_ACCESS_TOKEN_SECRET')
 
-get_fruit("banana")
+#get_fruit("banana")
 # Authenticate to Twitter , will add it when I am able to create a new twitter account and get the api keys
-#auth = tweepy.OAuthHandler(API_KEY, API_KEY_SECRET)
-#auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+auth = tweepy.OAuthHandler(API_KEY, API_KEY_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
-#api = tweepy.API(auth)
-
-#try:
-#    api.verify_credentials()
-#    print("Authentication OK")
-#except:
-#    print("Error during authentication")
+api = tweepy.API(auth)
+try:
+    api.verify_credentials()
+    print("Authentication OK")
+except:
+    print("Error during authentication")
