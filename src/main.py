dc70dcc17fd50c73ead6dcb195527631c1cd463c
EmilioRoche <32501779+EmilioRoche@users.noreply.github.com>
2022-10-20 21:43:29 -0600

    while loop tried to continuously tweet same tweet

--- src/main.py
+++ src/main.py
@@ -4,13 +4,13 @@ from config import create_api_object
 logger = logging.getLogger()
 
 def tweet_fruit(api):
-    api.update_status(get_fruit("banana"))
+    result = "banana has " + str(get_fruit("banana")) + " calories."
+    api.update_status(result)
 
 def main():
     api = create_api_object()
-    while True:
-        tweet_fruit(api)
-        logger.info("Waiting for tweet...")
+    tweet_fruit(api)
+    logger.info("Waiting for tweet...")
 
 
 if __name__ == "__main__":
